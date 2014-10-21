ee28273477443604d08c7d8b86e6f217f11d0ef9
44075 <44075@longroad.ac.uk>
2014-10-21 12:40:08 +0100

    hgkuygb

--- iteration task 3 revison tas.py
+++ iteration task 3 revison tas.py
@@ -3,8 +3,13 @@
 #Iteration task 3
 
 total = 0
-count = 0
-number = 0
+number = 1
 
 while number < 0:
+    number = int(input("Please input a number: "))
+    if number >= 0:
+        total = total + number
+print(total)
+
+
     
