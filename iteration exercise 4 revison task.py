29dfc474c1855c8d3e3bbf31a9be1ae2dcbd8c4f
44075 <44075@longroad.ac.uk>
2014-10-21 12:40:46 +0100

    kk

---
+++ iteration exercise 4 revison task.py
@@ -0,0 +1,19 @@
+#Callum Brown
+#21-10-14
+#Exercise 4
+
+number = 0
+while not 21 > number > 9:
+    number = int(input("Please enter a number: "))
+    if not 21 > number > 9:
+         print("Invalid")
+print("Valid")
+
+    
+    
+    
+
+
+
+
+ 
